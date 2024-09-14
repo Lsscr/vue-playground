86afe0b5f184034d49facf7b07e4fb4360df2eab
Luosr <luoshenran@gmail.com>
2024-09-14 14:40:56 +0800

    core： 修改配置

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig((command) => ({
   plugins: [vue(), UnoCSS()],
   base: command === 'build'
-    ? '/lsscr.github.io/'
+    ? '/vue-playground/'
     : '/'
 }));
