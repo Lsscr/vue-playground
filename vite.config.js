7a11bd75627ee14d25009b9fbd245f074b15ffc5
Luosr <luoshenran@gmail.com>
2024-09-14 14:09:45 +0800

    core： 修改配置

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), UnoCSS()],
-  base: '/lsscr/', // 替换为您的 GitHub 仓库名称
+  base: '/lsscr.github.io/', // 替换为您的 GitHub 仓库名称
 });
