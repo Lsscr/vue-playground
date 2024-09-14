e10f833f51d22df8aced5b089b5cbf3ccccc03cd
Luosr <luoshenran@gmail.com>
2024-09-14 14:32:34 +0800

    core: ci build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import UnoCSS from "unocss/vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((command) => ({
   plugins: [vue(), UnoCSS()],
-  base: './', // 替换为您的 GitHub 仓库名称
-});
+  base: command === 'build'
+    ? '/lsscr.github.io/'
+    : '/'
+}));
