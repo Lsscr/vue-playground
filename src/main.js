80ddf9fbc606204b2878d244fc4f6e57d3c1d6e0
Luosr <luoshenran@gmail.com>
2024-09-17 14:54:51 +0800

    core: 修改配置文件
    ```
    # 配置文件路径
    spring.config.location=classpath:application.yml
    ```

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import "uno.css";
-
+console.log('import.meta.env :>> ', import.meta.env);
 createApp(App).mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import UnoCSS from "unocss/vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig((command) => ({
-  plugins: [vue(), UnoCSS()],
-  base: command === 'build'
-    ? '/vue-playground/'
-    : '/'
-}));
+export default defineConfig(({ mode, command }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log('当前模式:', mode);
+  console.log('当前命令:', command);
+  console.log('环境变量:', env);
+  return {
+    plugins: [vue(), UnoCSS()],
+    base: env.VITE_BASE_URL,
+  };
+});
